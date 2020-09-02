3d4b4e819b8290e5c1723d3c53ad0d7718d358c9
bluewow <dogseen@gmail.com>
2020-09-02 13:49:58 +0900

    init

---
+++ src/main/java/de/bertilmuth/javadataclass/Main.java
@@ -0,0 +1,35 @@
+package de.bertilmuth.javadataclass;
+
+import java.io.File;
+import java.util.List;
+
+import de.bertilmuth.javadataclass.generate.JavaDataClassGenerator;
+import de.bertilmuth.javadataclass.model.ClassSpecification;
+import de.bertilmuth.javadataclass.read.YamlClassSpecificationReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// Make sure there is exactly one command line argument, the path to the YAML file
+		if (args.length != 1) {
+			System.out.println("Please supply exactly one argument, the absolute path of the YAML file.");
+			return;
+		}
+		
+		// Get the YAML file's handle, and the directory it's contained in
+		// (generated files will be placed there)
+		final String yamlFilePath = args[0];
+		final File yamlFile = new File(yamlFilePath);
+		final File outputDirectory = yamlFile.getParentFile();
+
+		// Step 1: Read in the YAML file, into class specifications
+		YamlClassSpecificationReader yamlReader = new YamlClassSpecificationReader();
+		List<ClassSpecification> classSpecifications = yamlReader.read(yamlFile);
+
+		// Step 2: Generate Java source files from the class specifications
+		JavaDataClassGenerator javaDataClassGenerator = new JavaDataClassGenerator();
+		javaDataClassGenerator.generateJavaSourceFiles(classSpecifications, outputDirectory);
+
+		System.out.println("Successfully generated files to: " + outputDirectory.getAbsolutePath());
+	}
+}
---
+++ src/test/java/de/bertilmuth/javadataclass/read/YamlReaderTest.java
@@ -0,0 +1,86 @@
+package de.bertilmuth.javadataclass.read;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.StringReader;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.bertilmuth.javadataclass.model.ClassSpecification;
+import de.bertilmuth.javadataclass.model.FieldSpecification;
+import de.bertilmuth.javadataclass.read.YamlClassSpecificationReader;
+
+public class YamlReaderTest {
+
+	@Test
+	public void readsEmptyClassSpecifications() {
+		String emptyClasses = "";
+		List<ClassSpecification> classSpecifications = readClassSpecifications(emptyClasses);
+		assertTrue(classSpecifications.isEmpty());
+	}
+	
+	@Test
+	public void readsSingleClassSpecificationWithoutFields() {
+		String classSpecificationString =  "Order:\r\n";
+		List<ClassSpecification> classSpecifications = readClassSpecifications(classSpecificationString);
+		assertEquals(1, classSpecifications.size());
+		assertEquals("Order", classSpecifications.get(0).getName());
+	}
+	
+	@Test
+	public void readsTwoClassSpecificationWithoutFields() {
+		String classSpecificationString =  "Order:\r\nCustomer:\r\n";
+		List<ClassSpecification> classSpecifications = readClassSpecifications(classSpecificationString);
+		assertEquals(2, classSpecifications.size());
+		assertEquals("Order", classSpecifications.get(0).getName());
+		assertEquals("Customer", classSpecifications.get(1).getName());
+	}
+	
+	@Test
+	public void readsSingleClassSpecificationWithSingleField() {
+		String classSpecificationString =  "Order:\r\n orderId: Long\r\n";
+		List<ClassSpecification> classSpecifications = readClassSpecifications(classSpecificationString);
+		List<FieldSpecification> orderFields = classSpecifications.get(0).getFieldSpecifications();
+		assertEquals(1, orderFields.size());
+		assertEquals("orderId", orderFields.get(0).getName());
+		assertEquals("Long", orderFields.get(0).getType());
+	}
+	
+	@Test
+	public void readsSingleClassSpecificationWithTwoFields() {
+		String classSpecificationString =  "Order:\r\n orderId: Long\r\n orderDate: Date";
+		List<ClassSpecification> classSpecifications = readClassSpecifications(classSpecificationString);
+		List<FieldSpecification> orderFields = classSpecifications.get(0).getFieldSpecifications();
+		assertEquals(2, orderFields.size());
+		assertEquals("orderId", orderFields.get(0).getName());
+		assertEquals("Long", orderFields.get(0).getType());
+		assertEquals("orderDate", orderFields.get(1).getName());
+		assertEquals("Date", orderFields.get(1).getType());
+	}
+	
+	@Test
+	public void readsTwoClassSpecificationsWithFields() {
+		String classSpecificationString =  "Order:\r\n orderId: Long\r\n orderDate: Date\r\nCustomer:\r\n firstName: String";
+		List<ClassSpecification> classSpecifications = readClassSpecifications(classSpecificationString);
+		List<FieldSpecification> orderFields = classSpecifications.get(0).getFieldSpecifications();
+		List<FieldSpecification> customerFields = classSpecifications.get(1).getFieldSpecifications();
+
+		assertEquals(2, orderFields.size());
+		assertEquals("orderId", orderFields.get(0).getName());
+		assertEquals("Long", orderFields.get(0).getType());
+		assertEquals("orderDate", orderFields.get(1).getName());
+		assertEquals("Date", orderFields.get(1).getType());
+		
+		assertEquals(1, customerFields.size());
+		assertEquals("firstName", customerFields.get(0).getName());
+		assertEquals("String", customerFields.get(0).getType());
+	}
+
+	private List<ClassSpecification> readClassSpecifications(String dataClassString) {
+		StringReader reader = new StringReader(dataClassString);
+		List<ClassSpecification> requirements = new YamlClassSpecificationReader().read(reader);
+		return requirements;
+	}
+}
